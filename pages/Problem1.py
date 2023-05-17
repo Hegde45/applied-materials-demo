9fbc4736cafc9fdc3487a59a4f5c160178fc3ff0
hegde45 <bharathegde45@gmail.com>
2023-05-17 18:55:05 +0530

    chagnes in requirements.txt

--- pages/Problem1.py
+++ pages/Problem1.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import streamlit as st
 from streamlit_pandas_profiling import st_profile_report
-import pandas_profiling as pp
+import ydata_profiling as yp
 
 st.set_page_config(layout="wide", page_title='Problem 1')
 
@@ -13,7 +13,7 @@ def load_csv():
 def app():
     st.title('**Problem 1 - Gold & Silver Exploratory Data Analysis**', anchor=False)
     gold_silver_data = load_csv()
-    profile_report = pp.ProfileReport(gold_silver_data, explorative=True)
+    profile_report = yp.ProfileReport(gold_silver_data, explorative=True)
     st.header('**Dataframe**')
     st.write(gold_silver_data)
     st_profile_report(profile_report)
