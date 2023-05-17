a907149be7bbfd6742cd48f8bf35fe1d33367485
hegde45 <bharathegde45@gmail.com>
2023-05-17 22:27:16 +0530

    deploy latest

--- pages/Problem3.py
+++ pages/Problem3.py
@@ -1,5 +1,9 @@
 import pandas as pd
 import streamlit as st
+import matplotlib.pyplot as plt
+import plotly.express as px
+import seaborn as sns
+import numpy as np
 
 st.set_page_config(layout="wide", page_title='Problem 3')
 
@@ -30,20 +34,82 @@ def app():
         del st.session_state['store_data']['Unnamed: 0']
 
     if 'Units Sold' not in st.session_state['sales_data']:
-        st.session_state['sales_data']['Units Sold'] = st.session_state['sales_data']['logmove'].apply(lambda x: round(pow(10, x)))
+        # Convert logmove column to numeric and apply exponential transform
+        st.session_state['sales_data']['logmove'] = pd.to_numeric(st.session_state['sales_data']['logmove'])
+        st.session_state['sales_data']['Units Sold'] = np.exp(st.session_state['sales_data']['logmove'])
+
+        # Check for outliers
+        st.write('units sold describe')
+        st.write(st.session_state['sales_data']['Units Sold'].describe())
 
     st.header("Sales Dataframe", anchor=False)
     st.dataframe(st.session_state['sales_data'], use_container_width=True)
     st.write("To get the number of units sold, an exponential transform has been applied to logmove column and resulting data has been written to **Units Sold** column")
 
+    st.header("Sales Summary Statistics", anchor=False)
+    st.dataframe(st.session_state['sales_data'].describe())
+
     st.header("Stores Dataframe", anchor=False)
     st.dataframe(st.session_state['store_data'], use_container_width=True)
+
+    st.header("Stores Summary Statistics", anchor=False)
+    st.dataframe(st.session_state['store_data'].describe())
 
     st.header("Joined Dataframe", anchor=False)
     merge_data = st.session_state['sales_data'].merge(st.session_state['store_data'], left_on='store', right_on='STORE', how='inner')
     if 'STORE' in merge_data:
         del merge_data['STORE']
     st.dataframe(merge_data, use_container_width=True)
+
+    st.header("Joined Summary Statistics", anchor=False)
+    st.dataframe(merge_data.describe())
+
+    # Distribution of sales
+    plt.figure(figsize=(10, 6))
+    sns.histplot(merge_data['Units Sold'], bins=20, kde=True)
+    plt.title('Distribution of Units Sold')
+    plt.xlabel('Units Sold')
+    plt.ylabel('Frequency')
+    st.pyplot(plt)
+
+    # Correlation heatmap
+    merge_data_copy = merge_data.copy()
+    if 'constant' in merge_data_copy:
+        del merge_data_copy['constant']
+    plt.figure(figsize=(60, 40), dpi=300)
+    sns.heatmap(merge_data_copy.corr(), annot=True, cmap='Blues', annot_kws={'size': 15})
+    plt.title('Correlation Heatmap', fontsize = 50)
+    plt.tick_params(axis = 'x', labelsize = 26)
+    plt.tick_params(axis = 'y', labelsize = 26)
+    plt.xticks(rotation=90)
+    plt.yticks(rotation=0)
+    plt.tight_layout()
+    st.pyplot(plt)
+
+    # Box plots
+    plt.figure(figsize=(60, 40), dpi=300)
+    sns.boxplot(data=merge_data, x='brand', y='Units Sold')
+    plt.xlabel('Brand')
+    plt.ylabel('Units Sold')
+    plt.title('Box Plot of Units Sold by Brand')
+    st.pyplot(plt)
+
+    # Scatter plot
+    plt.figure(figsize=(10, 6))
+    sns.scatterplot(data=merge_data, x='INCOME', y='Units Sold')
+    plt.xlabel('Income')
+    plt.ylabel('Units Sold')
+    plt.title('Scatter Plot of Units Sold vs Income')
+    st.pyplot(plt)
+
+    # Bar plot
+    avg_units_sold_by_brand = merge_data.groupby('brand')['Units Sold'].mean()
+    plt.figure(figsize=(10, 6))
+    sns.barplot(x=avg_units_sold_by_brand.index, y=avg_units_sold_by_brand.values)
+    plt.xlabel('Brand')
+    plt.ylabel('Average Units Sold')
+    plt.title('Average Units Sold by Brand')
+    st.pyplot(plt)
 
 if __name__ == '__main__':
     app()
--- pages/Problem4.py
+++ pages/Problem4.py
@@ -15,6 +15,9 @@ def load_csv():
 
 if 'train_data' not in st.session_state:
     st.session_state['train_data'] = pd.DataFrame(load_csv())
+    for column in st.session_state['train_data'].columns:
+        if st.session_state['train_data'][column].dtype == 'object': 
+            st.session_state['train_data'][column]=st.session_state['train_data'][column].astype('category').cat.codes
 
 # Calculate summary statistics
 @st.cache_data
@@ -24,44 +27,40 @@ def summary_stats():
 
 # Display the correlation
 @st.cache_data
-def heatmap(threshold_flag):
+def heatmap_all_correlations():
 
     df = st.session_state['train_data'].copy()
 
-    for column in df.columns:
-        if df[column].dtype == 'object': 
-            df[column]=df[column].astype('category').cat.codes
-
     correlation_matrix = df.corr()
-    if threshold_flag == True:
-        threshold = 0.5
-        correlation_matrix = correlation_matrix[((correlation_matrix >= threshold) | (correlation_matrix <= -threshold)) & (correlation_matrix != 1.000)]
-
-    plt.figure(figsize=(60, 40), dpi = 1000)
-    sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', xticklabels=correlation_matrix.columns, yticklabels=correlation_matrix.columns)
-    plt.xticks(rotation=45)
-    plt.tick_params(axis = 'x', labelsize = 16)
-    plt.yticks(rotation=45)
-    plt.tick_params(axis = 'y', labelsize = 16)
+    plt.figure(figsize=(100, 60), dpi=400)
+    sns.heatmap(correlation_matrix, annot=True, cmap='Blues', xticklabels=correlation_matrix.columns, yticklabels=correlation_matrix.columns, cbar=1, square=1, annot_kws={'size': 15})
+    plt.tick_params(axis = 'x', labelsize = 26)
+    plt.tick_params(axis = 'y', labelsize = 26)
+    plt.xticks(rotation=90)
+    plt.yticks(rotation=0)
     plt.tight_layout()
     st.pyplot(plt)
 
-# @st.cache_data
-# def heatmap_high_correlations():
-#     df = st.session_state['train_data'].copy()
-#     for column in df.columns:
-#         if df[column].dtype == 'object': 
-#             df[column]=df[column].astype('category').cat.codes
+@st.cache_data
+def heatmap_high_correlations():
+    df = st.session_state['train_data'].copy()
+    for column in df.columns:
+        if df[column].dtype == 'object': 
+            df[column]=df[column].astype('category').cat.codes
             
-#     correlation_matrix = df.corr()
-   
-#     plt.figure(figsize=(50, 20))
-#     sns.heatmap(filtered_corr_df, annot=True, cmap='coolwarm', xticklabels=correlation_matrix.columns, yticklabels=correlation_matrix.columns, cbar=1, square=1, annot_kws={'size': 15})
-#     plt.xticks(rotation=90)
-#     plt.yticks(rotation=0)
+    correlation_matrix = df.corr()
+    threshold = 0.5
+    filtered_corr_df = correlation_matrix[((correlation_matrix >= threshold) | (correlation_matrix <= -threshold)) & (correlation_matrix != 1.000)]
 
-#     plt.tight_layout()
-#     st.pyplot(plt)
+    plt.figure(figsize=(60, 40), dpi=300)
+    sns.heatmap(filtered_corr_df, annot=True, cmap='Blues', xticklabels=correlation_matrix.columns, yticklabels=correlation_matrix.columns, cbar=1, square=1, annot_kws={'size': 15})
+    plt.tick_params(axis = 'x', labelsize = 18)
+    plt.tick_params(axis = 'y', labelsize = 18)
+    plt.xticks(rotation=90)
+    plt.yticks(rotation=0)
+
+    plt.tight_layout()
+    st.pyplot(plt)
 
 # Display the scatter plots
 @st.cache_data
@@ -81,17 +80,48 @@ def app():
     st.subheader('Summary Statistics', anchor=False)
     summary_stats()
 
-    # heat map
+    # # heat map
     st.subheader('All Correlations', anchor=False)
-    heatmap(False)
+    heatmap_all_correlations()
 
     st.subheader('High Correlations', anchor=False)
-    heatmap(True)
+    heatmap_high_correlations()
         
     # scatter plot
     st.subheader('Scatter plot', anchor=False)
     selected_features = st.multiselect('Select features to plot against sale price', st.session_state['train_data'].columns)
     scatter_plot(selected_features)
+
+    st.subheader('summary numeric', anchor=False)
+    numeric_cols = st.session_state['train_data'].select_dtypes(include=['int64', 'float64'])
+    summary_stats2 = numeric_cols.describe()
+    st.dataframe(summary_stats2)
+
+    st.subheader('summary categorical', anchor=False)
+    categorical_cols = st.session_state['train_data'].select_dtypes(include=['int8'])
+    summary_stats3 = categorical_cols.describe()
+    st.dataframe(summary_stats3)
+
+    # group 1 - scatter matrix plot
+    subset_variables_array = [
+        ['MSSubClass','MSZoning','LotFrontage','LotArea','Street','Alley','LotShape','LandContour','Utilities','LotConfig','LandSlope','Neighborhood']
+        ['Condition1','Condition2','BldgType','HouseStyle','OverallQual','OverallCond','YearBuilt','YearRemodAdd']
+        ['RoofStyle','RoofMatl','Exterior1st','Exterior2nd','MasVnrType','MasVnrArea','ExterQual','ExterCond']
+        ['Foundation','BsmtQual','BsmtCond','BsmtExposure','BsmtFinType1','BsmtFinSF1','BsmtFinType2','BsmtFinSF2','BsmtUnfSF','TotalBsmtSF']
+        ['Heating','HeatingQC','CentralAir','Electrical','1stFlrSF','2ndFlrSF','LowQualFinSF','GrLivArea']
+        ['BsmtFullBath','BsmtHalfBath', 'FullBath','HalfBath','BedroomAbvGr']
+        ['KitchenAbvGr','KitchenQual','TotRmsAbvGrd','Functional','Fireplaces','FireplaceQu']
+        ['GarageType','GarageYrBlt','GarageFinish','GarageCars','GarageArea','GarageQual','GarageCond','PavedDrive','WoodDeckSF']
+        ['OpenPorchSF','EnclosedPorch','3SsnPorch','ScreenPorch','PoolArea','PoolQC','Fence','MiscFeature','MiscVal','MoSold','YrSold','SaleType','SaleCondition']
+    ]
+    for index, item in enumerate(subset_variables_array):
+        # Creating the scatter matrix plot
+        name = 'Scatter matrix plot - Category ' + str(index + 1)
+        st.subheader(name, anchor=False)
+        subset_df = st.session_state['train_data'][item]
+        sns.set(style="ticks")
+        sns.pairplot(subset_df)
+        st.pyplot(plt)
 
 if __name__ == '__main__':
     app()
